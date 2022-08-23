f3c5ab742cca6a82d6ef737ac52e74731ff754e5
Tom <tom.shacham@collectivebenefits.com>
2022-08-23 10:19:20 +0100

    update run.sh to download nvm if not present

--- typescript/run.sh
+++ typescript/run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if which nvm; then
-  echo got nvm
-  nvm install && nvm use && npm install --save
-else
-  NVM_DIR="$HOME/.nvm"
+if [[ ! -f ~/.nvm/nvm.sh ]]
+then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    NVM_DIR="$HOME/.nvm"
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  nvm install && nvm use && npm install --save
+else
+    source ~/.nvm/nvm.sh
 fi
+
+nvm install && nvm use && npm install --save
